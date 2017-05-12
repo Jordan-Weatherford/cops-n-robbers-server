b67c45305daed9e76f549b425228a1fcfd9805c1
Jordan-Weatherford <jrw821@gmail.com>
2017-05-11 18:44:43 -0700

    so much work

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,13 @@
 class LocationsController < ApplicationController
+    skip_before_filter  :verify_authenticity_token
+    @@coordinates = Hash.new
+
     def getData
-        coordinates = {
-            "test" => "something",
-            "test2" => "ldjfalksdj"
-        }
-        render :json => coordinates
+        render :json => @@coordinates
     end
 
     def sendData
+        @@coordinates[params[:key][0]] = [params[:key][1], params[:key][2]]
+        render :json => {"status" => "true"}
     end
 end
