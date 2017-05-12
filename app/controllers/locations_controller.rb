d292e7cc083d82ab5637470208f9406bd8b904b6
Jordan-Weatherford <jrw821@gmail.com>
2017-05-12 11:09:12 -0700

    added reset button and hiderFlag

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,7 @@
 class LocationsController < ApplicationController
     skip_before_filter  :verify_authenticity_token
     @@coordinates = Hash.new
+    @@hiderFlag = ""
 
     def getData
         render :json => @@coordinates
@@ -9,5 +10,18 @@ class LocationsController < ApplicationController
     def sendData
         @@coordinates[params[:key][0]] = [params[:key][1], params[:key][2]]
         render :json => {"status" => "true"}
+    end
+
+    def hiderFlag
+        if @@hiderFlag.empty?
+            @@hiderFlag = params[:key]
+            render :json => {"status" => true}
+        end
+        render :json => {"status" => false}
+    end
+
+    def reset
+        @@coordinates.clear
+        @@hiderFlag = ""
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
  get 'getData' => 'locations#getData'
  post 'sendData' => 'locations#sendData'
+ post 'hiderFlag' => 'locations#hiderFlag'
+ get 'reset' => 'locations#reset'
 end
